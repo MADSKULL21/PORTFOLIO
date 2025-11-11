40a5a90f0309ee88134a131d97c5f59c08aed007
Shaunak Lad <madskull@Shaunaks-MacBook-Air.local>
2025-11-11 22:06:56 +0530

    Move images to public folder for proper bundling in production

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,12 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+      build: {
+        assetsDir: 'assets',
+        rollupOptions: {
+          input: '/index.html',
+        }
       }
     };
 });
